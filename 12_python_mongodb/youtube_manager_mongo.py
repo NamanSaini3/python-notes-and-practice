ef7c9e3b1db9af4ba589344311b42eeb52b7dd00
Naman Saini <namansaini0003@gmail.com>
2026-06-28 16:52:14 +0530

    Update youtube_manager_mongo.py

--- 12_python_mongodb/youtube_manager_mongo.py
+++ 12_python_mongodb/youtube_manager_mongo.py
@@ -5,8 +5,6 @@ from pymongo import MongoClient
 
 
 
-client = MongoClient("mongodb+srv://youtubepy:youtubepy@cluster0.jvlihyv.mongodb.net/ytmanger")
-
 # Not a good idea to include id and password in the code, but for this example, we will do it anyway. 
 # In a real application, you should use environment variables or a configuration file to store sensitive information.
 
